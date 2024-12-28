aeb5234e959894c2651d876e6751a0671f8cda2f
games-on-web <buldik1111@gmail.com>
2024-12-28 22:17:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function increaseBPM() {
 
 // Initialize background music and loop it quietly
 function initBgMusic() {
-    bgAudio.volume = 0.1; // Set volume to 10% for quiet background music
+    bgAudio.volume = 0.7; // Set volume to 10% for quiet background music
     bgAudio.loop = true; // Loop the music indefinitely
     bgAudio.play();
 }
